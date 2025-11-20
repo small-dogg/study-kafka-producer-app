67e3dc65d14926dd094bad7a70dd3ef16c0bbc04
small-dogg <sy920112@naver.com>
2025-11-20 12:02:25 +0900

    chore: Init

---
+++ src/main/java/com/smalldgg/studykafkaproducerapp/StudyKafkaProducerAppApplication.java
@@ -0,0 +1,13 @@
+package com.smalldgg.studykafkaproducerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyKafkaProducerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyKafkaProducerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smalldgg/studykafkaproducerapp/StudyKafkaProducerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smalldgg.studykafkaproducerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyKafkaProducerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
