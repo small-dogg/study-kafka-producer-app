bc11877c11e355675507b26b674e0e4a5a7d21b5
small-dogg <sy920112@naver.com>
2025-12-19 14:30:56 +0900

    feat: 카프카 통신구간 내 전송 실패시 다시 시도될 수 있도록 스케줄러 정의

--- src/main/java/com/smalldgg/studykafkaproducerapp/event/impression/ImpressionEventRepository.java
+++ src/main/java/com/smalldgg/studykafkaproducerapp/event/impression/ImpressionEventRepository.java
@@ -3,11 +3,25 @@ package com.smalldgg.studykafkaproducerapp.event.impression;
 import com.smalldgg.studykafkaproducerapp.event.enums.EventStatus;
 import com.smalldgg.studykafkaproducerapp.event.impression.entity.ImpressionEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ImpressionEventRepository extends JpaRepository<ImpressionEvent, UUID> {
 
     Optional<ImpressionEvent> findByIdAndEventStatus(UUID id, EventStatus eventStatus);
+
+    @Query(value = """
+            SELECT 
+                impe
+            FROM ImpressionEvent impe
+            WHERE impe.eventStatus = 'PENDING'
+            ORDER BY impe.createTime DESC
+            LIMIT 100
+            FOR UPDATE skip locked
+        """, nativeQuery = true
+    )
+    List<ImpressionEvent> findImpressionEventsWithSkipLock();
 }
---
+++ src/main/java/com/smalldgg/studykafkaproducerapp/worker/ImpressionEventSender.java
@@ -0,0 +1,38 @@
+package com.smalldgg.studykafkaproducerapp.worker;
+
+import com.smalldgg.studykafkaproducerapp.event.impression.ImpressionEventRepository;
+import com.smalldgg.studykafkaproducerapp.event.impression.entity.ImpressionEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class ImpressionEventSender {
+
+    private static final String TOPIC = "impression";
+    private final ImpressionEventRepository impressionEventRepository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Scheduled(fixedRate = 1000)
+    public void sendImpressionEvent() {
+        List<ImpressionEvent> impressionEvents = impressionEventRepository.findImpressionEventsWithSkipLock();
+
+        if (!CollectionUtils.isEmpty(impressionEvents)) {
+            for (ImpressionEvent event : impressionEvents) {
+                kafkaTemplate.send(TOPIC, String.valueOf(event.getId()), event.getPayload())
+                        .whenComplete((res, ex) -> {
+                            if (ex == null) {
+                                event.markSuccess();
+                            } else {
+                                event.countUp();
+                            }
+                        });
+            }
+        }
+    }
+}
